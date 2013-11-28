0458b27b6e80059ac87e874bcf1496aa74eb06af
seavers <seavers@163.com>
2013-11-28 19:00:16 +0800

    Create 2013-11-27-xianjian.markdown

---
+++ hook.rb
@@ -0,0 +1,20 @@
+require 'socket'
+  
+server = TCPServer.open 4001 
+puts "Listening on port " + server.addr[1].to_s
+  
+loop {
+  client = server.accept()
+  while((x = client.gets) != "\r\n")
+    puts x
+  end
+
+  client.puts "HTTP/1.1 200 OK\r\n\r\nOK"
+  client.close
+  puts 'OK'
+
+  system('git pull')
+  system('rake generate')
+  puts ''
+}
+
