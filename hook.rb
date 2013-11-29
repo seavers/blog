7f6b907d5c3af39ceedb3d3f2974b50fe353a64e
seavers <seavers@163.com>
2013-11-29 13:22:39 +0800

    config CNAME hook

---
+++ hook.rb
@@ -0,0 +1,23 @@
+require 'socket'
+  
+server = TCPServer.open 4001 
+puts "Listening on port " + server.addr[1].to_s
+  
+loop {
+  client = server.accept()
+
+  puts Time.now
+  while((x = client.gets) != "\r\n")
+    puts x
+  end
+
+  client.puts "HTTP/1.1 200 OK\r\n\r\nOK"
+  client.close
+  puts 'OK'
+
+  system('git pull')
+  system('rake generate')
+  system('rake deploy')			##be careful  hook.loop
+  puts ''
+}
+
