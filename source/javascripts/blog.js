82ea0bda969bc734b793745a44d0e9e6ef0d3ed5
seavers <seavers@163.com>
2016-01-17 17:16:23 +0800

    mybatis-dongtai

--- source/javascripts/blog.js
+++ source/javascripts/blog.js
@@ -1,3 +1,11 @@
+//处理客户端相关的
+if(location.href.indexOf('webView=true') > -1) {
+	jQuery('body>header,body>nav').hide();
+}
+
+
+
+//微博相关的
 WB2.anyWhere(function(W){
     W.widget.connectButton({
         id: "wb_connect_btn",
